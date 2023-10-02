7c8190452934f9dc785b0a5329dd351f2fc1dfc0
Brian <brian.kawa@student.moringaschool.com>
2023-10-02 11:23:54 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16
+const num4 = 4
+const mod = num3 / num4;
+
+const max = Math.max(2, 4, 4, 8, 10, 12, 14, 16, 18, 20);
